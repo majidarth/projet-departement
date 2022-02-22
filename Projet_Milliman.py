5e25c745c6296cf6a2659e7de9acf6fc156ff76d
Alexandre-Wan <81426235+Alexandre-Wan@users.noreply.github.com>
2022-02-22 14:50:50 +0100

    Update Projet_Milliman.py

--- Projet_Milliman.py
+++ Projet_Milliman.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 
 # Constantes du problème
-d = 10
+d = 3
 S0 = np.ones(d) * 100.
 r = 0.1
 vol = 0.2
@@ -37,14 +37,13 @@ def polynomial_reg(t, T , S0, r, gamma, d, K, n_paths, vol):
     S_t = blackscholes_mc(0, t, n_paths, S0, vol, r, gamma, d)
     S_T = np.zeros_like(S_t)
     for i in range (len(S_t)):
-        S_T[i] = blackscholes_mc(t, T, 1, S0, vol, r, gamma, d)[0]
+        S_T[i] = blackscholes_mc(t, T, 1, S_t[i], vol, r, gamma, d)[0]
     V_t = np.exp(-r*(T-t)) * np.maximum(np.prod(S_T, axis = 1)**(1/d)-K,0)
+
     p = np.polyfit(S_t, V_t, deg =2)
     return p
     
     
 if __name__ == '__main__':
-
-    print(S0)
     print(polynomial_reg(0.5, T , S0, r, gamma, d, 100, 10, vol))
     
